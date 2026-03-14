a723cb30dd4a6bc4a04524a75c1ad24e5fbb0870
AlittleJuku <109045201+amashriq@users.noreply.github.com>
2026-03-14 17:59:30 +0100

    added live schedule

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,6 +1,8 @@
 import { supabase } from "@/lib/supabase";
 import EventList from "./EventList";
 
+export const revalidate = 0;
+
 export default async function SchedulePage() {
   const { data: events } = await supabase.from("events").select("*");
 
