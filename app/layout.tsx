845460d19473af78e13776a8b0b9d9cc30f8a203
AlittleJuku <109045201+amashriq@users.noreply.github.com>
2026-05-12 19:53:16 +0200

    small things

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,14 +59,12 @@ export default function RootLayout({
       >
         <a
           href='#main-content'
-          className='sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[100] focus:px-4 focus:py-2 focus:bg-white focus:text-black focus:font-bold focus:text-sm focus:uppercase focus:tracking-widest focus:shadow-md'
+          className='sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-100 focus:px-4 focus:py-2 focus:bg-white focus:text-black focus:font-bold focus:text-sm focus:uppercase focus:tracking-widest focus:shadow-md'
         >
           Skip to main content
         </a>
         <Navbar />
-        <div id='main-content'>
-          {children}
-        </div>
+        <div id='main-content'>{children}</div>
         <Analytics />
       </body>
     </html>
